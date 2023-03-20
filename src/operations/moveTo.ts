b8a7a0206be0d65bd3f788ccfcf91ca59dd8c30f
Richard Yu <vipzhicheng@gmail.com>
2023-03-20 10:14:28 +0800

    wip: optimize history

--- src/operations/moveTo.ts
+++ src/operations/moveTo.ts
@@ -4,8 +4,9 @@ import { getSettings } from '@/common/funcs';
 
 export default () => {
   const keyBindings = getSettings('keyBindings');
-  const handler = async ({ uuid }) => {
+  const openMoveBlockWindow = async ({ uuid }) => {
     const targetStore = useTargetStore();
+
     if (uuid) {
       targetStore.setFallbackUUID(uuid);
     }
@@ -24,8 +25,27 @@ export default () => {
         binding: keyBindings.moveBlock,
       },
     },
-    handler
+    openMoveBlockWindow
   );
-  logseq.Editor.registerSlashCommand(`Move Block`, handler);
-  logseq.Editor.registerBlockContextMenuItem(`Move Block`, handler);
+  logseq.Editor.registerSlashCommand(`Move Block`, openMoveBlockWindow);
+  logseq.Editor.registerBlockContextMenuItem(`Move Block`, openMoveBlockWindow);
+  // Register icon ui
+  logseq.provideModel({
+    async openMoveBlock() {
+      const block = await logseq.Editor.getCurrentBlock();
+      if (!block || !block.uuid) {
+        logseq.UI.showMsg('Need to select one block at least!', 'warning');
+        return;
+      }
+      await openMoveBlockWindow({ uuid: block.uuid });
+    },
+  });
+  logseq.App.registerUIItem('pagebar', {
+    key: 'logseq-move-block',
+    template: `
+     <a class="button" data-on-click="openMoveBlock" title="Open move block window">
+      <i class="ti ti-corner-up-right" style=""></i>
+     </a>
+    `,
+  });
 };
